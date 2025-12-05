bce95b6aaae8e983612732ea879f23c96db64e86
DavidGinovyan25 <david48952@gmail.com>
2025-12-05 15:23:18 +0300

    fix: remove structure(Station) and field(RoutePoint->stations)

--- lib/RouteClasses/RouteClasses.hpp
+++ lib/RouteClasses/RouteClasses.hpp
@@ -1,20 +1,9 @@
-#include <iostream> 
+#include <iostream>
 #include <string>
 #include <vector>
-
-struct Station {
-    double latitude;
-    double longitude;
-    std::string code;
-};
 
 struct RoutePoint {
     std::string name;
     std::string code;
-    std::vector<Station> stations;
     RoutePoint(std::string name) : name(name) {}
 };
-
-inline std::ostream& operator<<(std::ostream& os, const Station& s) {
-    return os << "Station " << s.latitude << " " << s.longitude << " " << s.code;
-}
