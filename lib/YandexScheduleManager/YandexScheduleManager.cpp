ebc8d2a7f49c8c84df0605025c77487ba9689f1f
DavidGinovyan25 <david48952@gmail.com>
2025-12-05 16:43:08 +0300

    fix: remove func(FilterStationsByDate)
    ref: rename func(FetchStations->FindPointsCode)
    
    fix(FindPointsCode): delete cicle proccesing

--- lib/YandexScheduleManager/YandexScheduleManager.cpp
+++ lib/YandexScheduleManager/YandexScheduleManager.cpp
@@ -3,20 +3,20 @@
 std::expected<json, std::string> YandexScheduleManager::GetJsonFromUrl(std::string url) {
     cpr::Response r = cpr::Get(cpr::Url{url});
     HttpErrorHandler http_error;
-    if (r.error.code != cpr::ErrorCode::OK) 
+    if (r.error.code != cpr::ErrorCode::OK)
         return std::unexpected(http_error.HandleNetworkErrors(r));
-    if (r.status_code != static_cast<int>(http_error.StatusCode::OK))
+    if (r.status_code != static_cast<int>(HttpErrorHandler::StatusCode::OK))
         return std::unexpected(http_error.HandleStatusCodeErrors(r));
     try {
         return json::parse(r.text);
-    } catch (const json::parse_error& http_error) {
-        return std::unexpected(std::string("JSON parse error: ") + http_error.what());
+    } catch (const json::parse_error& e) {
+        return std::unexpected(std::string("JSON parse error: ") + e.what());
     } catch (...) {
         return std::unexpected("Unknown JSON error");
     }
 }
 
-bool YandexScheduleManager::FetchStations(RoutePoint& departure, RoutePoint& arrival) {
+void YandexScheduleManager::FindPointsCode(RoutePoint& departure, RoutePoint& arrival) {
     auto result = GetJsonFromUrl(
         "https://api.rasp.yandex-net.ru/v3.0/stations_list/"
         "?apikey=dbfeef6a-2419-43b8-83fd-66d8e630dbbd"
@@ -24,9 +24,11 @@ bool YandexScheduleManager::FetchStations(RoutePoint& departure, RoutePoint& arr
         "&format=json"
     );
     if (!result) {
-        std::cerr << result.error() << std::endl;
-        return false;
+        std::cerr << "FetchStations: " + result.error() << std::endl;
+        return;
     }
+    bool departure_flag = false;
+    bool arrival_flag = false;
     json all_stations = *result;
     for (auto& country : all_stations["countries"]) {
         for (auto& region : country["regions"]) {
@@ -42,86 +44,70 @@ bool YandexScheduleManager::FetchStations(RoutePoint& departure, RoutePoint& arr
                 auto settlement_title = settlement["title"].get<std::string>();
                 auto settlement_code = settlement["codes"]["yandex_code"].get<std::string>();
 
-                if (departure.name == settlement_title) 
-                    departure.code = settlement_code;
-                if (arrival.name == settlement_title) 
-                    arrival.code = settlement_code;
+                auto compare_title = [&](bool& flag, const std::string& name, std::string& code){
+                    if (!flag && name == settlement_title) {
+                        code = settlement_code;
+                        flag = true;}};
 
-                for (auto& station : settlement["stations"]) { 
-                    if (!station.contains("latitude") || !station["latitude"].is_number())
-                        continue;
-                    if (!station.contains("longitude") || !station["longitude"].is_number())
-                        continue;
-                    if (!station.contains("codes") || station["codes"].is_string())
-                        continue;
-                    if (!station["codes"].contains("yandex_code") || station["codes"]["yandex_code"].is_null())
-                        continue;
-
-                    auto push_station = [&](RoutePoint& point) {
-                        if (point.name == settlement_title)
-                            point.stations.push_back(Station{
-                                .latitude = station["latitude"], 
-                                .longitude = station["longitude"],
-                                .code = station["codes"]["yandex_code"]});
-                            };    
-                    push_station(departure);
-                    push_station(arrival);
-                }
+                compare_title(departure_flag, departure.name, departure.code);
+                compare_title(arrival_flag, arrival.name, arrival.code);
+                if (departure_flag && arrival_flag)
+                    break;
             }
+            if (departure_flag && arrival_flag)
+                break;
         }
+        if (departure_flag && arrival_flag)
+            break;
     }
-    return true;
+    if (!departure_flag)
+        std::cerr << "Error: Unknown settlement " + departure.name  << std::endl;
+    if (!arrival_flag)
+        std::cerr << "Error: Unknown settlement " + arrival.name  << std::endl;
 }
 
-bool YandexScheduleManager::FilterStationsByDate(RoutePoint& departure, std::string departure_date) {
-    for (int i = departure.stations.size() - 1; i >= 0; --i) {
-        auto result = GetJsonFromUrl(
-            "https://api.rasp.yandex-net.ru/v3.0/schedule/"
-            "?apikey=dbfeef6a-2419-43b8-83fd-66d8e630dbbd"
-            "&station=" + departure.stations[i].code +
-            "&date=" + departure_date +
-            "&transfers=true"
-        );
-        if (!result) {
-            std::cerr << result.error() << std::endl;
-            return false;
-        }
-        json route_schedule_request = *result;
-        if (route_schedule_request.is_null())
-            departure.stations.erase(departure.stations.begin() + i);
-    }
-    return true;
-}
-
-
-bool YandexScheduleManager::SearchRoutes(
-            RoutePoint& departure, 
+void YandexScheduleManager::SearchRoutes(
+            RoutePoint& departure,
             RoutePoint& arrival,
             std::string departure_date) {
-                
-    for (Station& d_s : departure.stations) {
-        for (Station& a_s : arrival.stations) {
-            auto result  = GetJsonFromUrl(
-                "https://api.rasp.yandex-net.ru/v3.0/search/"
-                "?apikey=dbfeef6a-2419-43b8-83fd-66d8e630dbbd"
-                "&from=" + d_s.code + 
-                "&to=" + a_s.code + 
-                "&page=1"
-                "&date=" + departure_date
-            );
-            if (!result) {
-                std::cerr << result.error() << std::endl;
-                return false;
-            }
-            json route_schedule_request = *result; 
-            if (route_schedule_request.is_null())
-                continue;
-            for (auto& segments : route_schedule_request["segments"]) {
-                if (segments["has_transfers"] == true && segments["thread"].size() > 2) //FIX THIS 
-                    continue; 
-                std::cout << segments["from"]["title"] << " " << segments["to"]["title"];
-            }
-        }
+
+    auto result = GetJsonFromUrl(
+        "https://api.rasp.yandex-net.ru/v3.0/search/"
+        "?apikey=dbfeef6a-2419-43b8-83fd-66d8e630dbbd"
+        "&from=" + departure.code +
+        "&to=" + arrival.code +
+        "&page=1"
+        "&date=" + departure_date +
+        "&transfers=true"
+    );
+    if (!result) {
+        std::cerr << "SerachRoutes: " + result.error() << std::endl;
+        return;
     }
-    return true;
+    json searched_routes = *result;
+    std::ofstream file("data.json");
+    file << searched_routes.dump(4);
+    int kMaxCountTransfers = 1;
+    int kIndexNormalization = 2;
+    for (auto& segments : searched_routes["segments"]) {
+        bool has_transfers = segments["has_transfers"];
+        if (!has_transfers) {
+            std::cout << "================================================================" << std::endl;
+            std::cout << segments["from"]["title"] << ": " << segments["departure"] << std::endl;
+            std::cout << segments["to"]["title"] << ": " << segments["arrival"] << std::endl;
+            std::cout << "================================================================" << std::endl;
+            continue;
+        }
+        if (segments["details"].size() / kIndexNormalization > kMaxCountTransfers)
+            continue;
+        std::cout << "================================================================" << std::endl;
+        std::cout << segments["details"][0]["from"]["title"] << ": " << segments["details"][0]["departure"] << std::endl;
+        std::cout << segments["details"][0]["to"]["title"] << ": " << segments["details"][0]["arrival"] << std::endl ;
+        std::cout << "----------------------------------------------------------------" << std::endl;
+        std::cout << segments["details"][1 * kIndexNormalization]["from"]["title"] << ": "
+            << segments["details"][1 * kIndexNormalization]["departure"] << std::endl;
+        std::cout << segments["details"][1 * kIndexNormalization]["to"]["title"] << ": "
+            << segments["details"][1 * kIndexNormalization]["arrival"] << std::endl ;
+        std::cout << "================================================================" << std::endl;
+    }
 }
