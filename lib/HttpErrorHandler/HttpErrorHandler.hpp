8a7ab7fc63897aabfcb9ea2f8ad9bdce3b707695
DavidGinovyan25 <david48952@gmail.com>
2026-02-03 13:19:26 +0300

    ref: add space

--- lib/HttpErrorHandler/HttpErrorHandler.hpp
+++ lib/HttpErrorHandler/HttpErrorHandler.hpp
@@ -5,14 +5,14 @@
 class HttpErrorHandler {
 public:
     enum class StatusCode {
-        OK = 200,
-        BAD_REQUEST = 400,
-        UNAUTHORIZED = 401,
-        FORBIDDEN = 403,
-        NOT_FOUND = 404,
-        TOO_MANY_REQUEST = 429,
-        INTERNAL_SERVER_ERROR = 500,
-        SERVICE_UNAVAILABLE = 503
+      OK = 200,
+      BAD_REQUEST = 400,
+      UNAUTHORIZED = 401,
+      FORBIDDEN = 403,
+      NOT_FOUND = 404,
+      TOO_MANY_REQUEST = 429,
+      INTERNAL_SERVER_ERROR = 500,
+      SERVICE_UNAVAILABLE = 503
     };
     std::string HandleNetworkErrors(cpr::Response& r);
     std::string HandleStatusCodeErrors(cpr::Response& r);
