e34814464c1b6f8542365b72b2d157e9dcac91a1
DavidGinovyan25 <david48952@gmail.com>
2026-02-03 13:24:03 +0300

    fix: corrected logic

--- bin/main.cpp
+++ bin/main.cpp
@@ -1,87 +1,44 @@
-#include <RouteCache.hpp>
+#include "YandexScheduleManager.hpp"
 
-#include <iostream>
-#include <fstream>
-
-using json = nlohmann::json;
-
-auto print_string = [](std::string s){std::cout << s << std::endl;};
-
-template <typename T>
-auto print = [](T s){std::cout << s << std::endl;};
-
-
-
-struct Station {
-    double latitude;
-    double longitude;
-    std::vector<std::string> codes;
+class Runner {
+public:
+    void Run(int argc, char* argv[]);
 };
 
-std::ostream& operator<<(std::ostream& os, const Station& s) {
-    return os << "Station " << s.latitude << " " << s.longitude;
-}
-
-
-json GetJsonFromUrl(std::string url) {
-    cpr::Response r = cpr::Get(cpr::Url{url});
-    if (r.status_code != 200) {
-        print_string("UNSUCCESS!");
-        return json();
-    }
-    print_string("SUCCESS!");
-    return json::parse(r.text);
-}
-
-struct SettlementInfo {
-    std::string name;
-    std::string code;
-    std::vector<Station> stations;
-    SettlementInfo(std::string name) : name(name) {}
-};
-
-int main(int argc, char *argv[]) {
-    SettlementInfo departure(argv[1]);
-    SettlementInfo arrival(argv[2]);
+void Runner::Run(int argc, char* argv[]) {
+    RoutePoint departure(argv[1]);
+    RoutePoint arrival(argv[2]);
     std::string departure_date = argv[3];
 
-    std::ofstream json_file("data.json"); 
-    json all_stations = GetJsonFromUrl(
-        "https://api.rasp.yandex-net.ru/v3.0/stations_list/?apikey=dbfeef6a-2419-43b8-83fd-66d8e630dbbd&lang=ru_RU&format=json"
-    );
+    YandexScheduleManager api_requester;
 
+    api_requester.cache_manager.LoadFromFileSystem();
+    api_requester.FindPointsCode(departure, arrival);
+    api_requester.SearchRoutes(departure, arrival, departure_date);
+    api_requester.cache_manager.SaveInFileSystem();
+}
 
-    for (int i = 0; i < all_stations["countries"].size(); ++i) {
-        for (int j = 0; j < all_stations["countries"][i]["regions"].size(); ++j) {
-            for (int k = 0; k < all_stations["countries"][i]["regions"][j]["settlements"].size(); ++k) {
-                auto current_settlement = all_stations["countries"][i]["regions"][j]["settlements"][k];
-                for (int l = 0; l < current_settlement["stations"].size(); ++l) { 
-                    auto current_station = all_stations["countries"][i]["regions"][j]["settlements"][k]["stations"][l];
-
-                    if (!current_settlement.contains("title") || !current_settlement["title"].is_string())
-                        continue;
-                    if (!current_station.contains("latitude") || !current_station["latitude"].is_number())
-                        continue;
-                    if (!current_station.contains("longitude") || !current_station["longitude"].is_number())
-                        continue;
-                    
-                    auto push_station = [&](const std::string& name, std::vector<Station>& stations){
-                        if (name == current_settlement["title"])
-                            stations.push_back(Station{
-                            .latitude = current_station["latitude"], 
-                            .longitude = current_station["longitude"]
-                        });
-                    };
-                    push_station(departure.name, departure.stations);
-                    push_station(arrival.name, arrival.stations);
-                }
-            }
-        }
-    }
-
-    json_file << all_stations.dump(4);
+int main(int argc, char *argv[]) {
+    Runner runner;
+    runner.Run(argc, argv);
     return 0;
 }
 
 
+    // std::string url1 =
+    //     "https://api.rasp.yandex-net.ru/v3.0/stations_list/"
+    //     "?apikey=dbfeef6a-2419-43b8-83fd-66d8e630dbbd"
+    //     "&lang=ru_RU"
+    //     "&format=json";
+    // // std::cout << api_requester.cache_manager.storage[url1].first.fixed_time << std::endl;
+    // std::string url =
+    //     "https://api.rasp.yandex-net.ru/v3.0/search/"
+    //     "?apikey=dbfeef6a-2419-43b8-83fd-66d8e630dbbd"
+    //     "&from=" + departure.code +
+    //     "&to=" + arrival.code +
+    //     "&page=1"
+    //     "&date=" + departure_date +
+    //     "&transfers=true";
 
+    // std::cout << api_requester.cache_manager.order.size() << std::endl;
+    // std::cout << api_requester.cache_manager.storage.size() << std::endl;
